4a06fc8a356de554e1c64bf76f8b9567761b5f0c
jovinasnascimento-netizen <jovinas.nascimento@gmail.com>
2026-05-09 18:59:07 -0300

    Atualizar o exerc5.c

--- LISTA5/exerc5.c
+++ LISTA5/exerc5.c
@@ -12,3 +12,36 @@ int main(){
       printf("ACESSO LIBERADO");
         return 0;
 }
+
+COM VERIFICAÇÃO 
+
+
+#include <stdio.h>
+
+int main() {
+
+    int codigo;
+    int verificacao;
+
+    printf("Digite o codigo de acesso: ");
+    scanf("%d", &codigo);
+
+    while(codigo != 4321) {
+        printf("Codigo %d esta incorreto, digite novamente.\n", codigo);
+        scanf("%d", &codigo);
+    }
+
+    printf("Digite o codigo de verificacao: ");
+    scanf("%d", &verificacao);
+
+    while(verificacao != 10) {
+        printf("ACESSO NEGADO\n");
+
+        printf("Digite novamente: ");
+        scanf("%d", &verificacao);
+    }
+
+    printf("ACESSO LIBERADO\n");
+
+    return 0;
+}
